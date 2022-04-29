c027a7246cb720257a3224d20d71723bd3d8207e
tigerchen04 <104207588+tigerchen04@users.noreply.github.com>
2022-04-29 08:35:40 +0200

    cpp-Datei erstellt und allgemeinen Konstruktor definiert

---
+++ Klasse_Datum.cpp
@@ -0,0 +1,12 @@
+#include "Klasse_Datum.h"
+#include <iostream>
+using namespace std;
+
+//allgemeinen Konstruktor definieren: 
+Datum::Datum(int A, int B, int C)
+{
+	int tag = A;
+	int monat = B;
+	int jahr = C;
+	
+}
