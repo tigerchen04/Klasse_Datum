beebf37949dd2d9e5a2aaa969916a650fb3857f2
tigerchen04 <104207588+tigerchen04@users.noreply.github.com>
2022-04-29 09:23:07 +0200

    Methoden IstSchaltjahr und TagDesJahres definiert

--- Klasse_Datum.cpp
+++ Klasse_Datum.cpp
@@ -25,3 +25,27 @@ int oktober = 31;
 int november = 30;
 int dezember = 31;
 
+int Datum::IstSchaltjahr(int irgendeinJahr)
+{
+	if ((irgendeinJahr % 4 == 0) && (irgendeinJahr % 100 != 0))
+	{
+		return 1;
+	}
+	else if (irgendeinJahr % 400 == 0)
+	{
+		return 0;
+	}
+
+}
+
+int Datum::TagDesJahres(int tag, int monat, int jahr)
+{
+	cout << "Tag eingeben:" << endl;
+	cin >> tag;
+	cout << "Monat eingeben:" << endl;
+	cin >> monat;
+	cout << "Jahr eingeben:" << endl;
+	cin >> jahr;
+
+	
+}
