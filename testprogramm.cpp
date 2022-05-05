8b5da0a045287c5d57a0b55472a5825cd6c60c89
PaulFrankowski <94003153+PaulFrankowski@users.noreply.github.com>
2022-05-05 17:19:07 +0200

    Testprogramm gebaut

--- testprogramm.cpp
+++ testprogramm.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-#include <"Klasse_Datum.h">
-#include<"Klasse_Datum.cpp">
+#include "Klasse_Datum.h"
+#include "Klasse_Datum.cpp"
 using namespace std;
 
 int main()
 {
+Datum Testdatum = Datum(2,2,2002);
 
-
-
-
-
-
-
-
-
-
-
-
+	//cout << "Geben Sie ein Jahr ein:";
+	//cin >> jahr;
+	cout << Testdatum.IstSchaltjahr(2002);
+	//cout << "Geben Sie den Tag, Monat und Jahr ein: ";
+		//cin >> tag;
+		//cin >> monat;
+		//cin >> jahr;
+	cout << Testdatum.TagDesJahres(2, 2, 2002);
 }
