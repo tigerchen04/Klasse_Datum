208616e63bec6cad7034633795fc2067658d3e3d
tigerchen04 <104207588+tigerchen04@users.noreply.github.com>
2022-05-06 09:01:14 +0200

    Update testprogramm.cpp

--- testprogramm.cpp
+++ testprogramm.cpp
@@ -7,12 +7,7 @@ int main()
 {
 Datum Testdatum = Datum(2,2,2002);
 
-	//cout << "Geben Sie ein Jahr ein:";
-	//cin >> jahr;
-	cout << Testdatum.IstSchaltjahr(2002);
-	//cout << "Geben Sie den Tag, Monat und Jahr ein: ";
-		//cin >> tag;
-		//cin >> monat;
-		//cin >> jahr;
+	cout << Testdatum.IstSchaltjahr(2002) << endl;
+
 	cout << Testdatum.TagDesJahres(2, 2, 2002);
 }
